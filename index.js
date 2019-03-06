5ab768613ac54f197ed027421204c44a7cba34b9
Chelsea Crowson <chelsea.crowson@me.com>
2019-03-06 14:19:28 -0600

    fixed store issues

--- index.js
+++ index.js
@@ -44,15 +44,45 @@ document.addEventListener('DOMContentLoaded', function () {
             screenDisplay = ''
             result = 0
             disp.innerText = result
-        } else if (parseFloat(val) || val.includes('.')) {
-            screenDisplay += val
-            disp.innerText = screenDisplay
+        } else if (parseFloat(val) || val == 0 || val.includes('.')) {
+            screenDisplay = ''
+            if (num1 == '') {
+                screenDisplay += val
+                disp.innerText = screenDisplay
+                num1 = screenDisplay
+            } else if (num2 == '') {
+                screenDisplay += val
+                num2 = screenDisplay
+                disp.innerText = num2
+            }
         } else if (val.includes('+') || val.includes('-') || val.includes('*') || val.includes('/')) {
             math = val
-            if (num1 == '') {
-                num1 = screenDisplay
-                screenDisplay = ''
+            if (num1 != '' && num2 != '') {
+                switch (math) {
+                    case '+':
+                        result = (parseFloat(num1) + parseFloat(num2))
+                        break;
+                    case '-':
+                        result = (parseFloat(num1) - parseFloat(num2))
+                        break;
+                    case '*':
+                        result = (parseFloat(num1) * parseFloat(num2))
+                        break;
+                    case '/':
+                        result = (parseFloat(num1) / parseFloat(num2))
+                        break;
+                }
+                num1 = result
+                num2 = ''
             }
+            // if (num1 == '') {
+            //     num1 = screenDisplay
+            //     screenDisplay = ''
+            // } else {
+            //     num2 = screenDisplay
+            //     screenDisplay = ''
+            //     console.log('here.............', screenDisplay)
+            // }
         } else if (val.includes('Delete')) {
             if (screenDisplay.length > 1) {
                 screenDisplay = screenDisplay.slice(0, -1)
@@ -61,39 +91,35 @@ document.addEventListener('DOMContentLoaded', function () {
                 disp.innerText = 0
             }
         } else if (val.includes('=')) {
-            num2 = screenDisplay
+            // num2 = screenDisplay
             screenDisplay = ''
             switch (math) {
                 case '+':
                     console.log('plus')
                     result = (parseFloat(num1) + parseFloat(num2))
-                    num1 = result
-                    num2 = ''
                     break;
                 case '-':
                     result = (parseFloat(num1) - parseFloat(num2))
-                    num1 = result
-                    num2 = ''
                     break;
                 case '*':
                     result = (parseFloat(num1) * parseFloat(num2))
-                    num1 = result
-                    num2 = ''
                     break;
                 case '/':
                     result = (parseFloat(num1) / parseFloat(num2))
-                    num1 = result
-                    num2 = ''
                     break;
             }
+            num1 = result
+            num2 = ''
         } else if (val.includes('store')) {
-            store = screenDisplay
+            store = disp.innerText
             disp.innerText = 'Stored!'
+            console.log(store)
         } else if (val.includes('recall')) {
-            disp.innerText = store
             num1 = store
             num2 = ''
             screenDisplay = ''
+            result = 0
+            disp.innerText = store
         }
 
         if (parseFloat(result)) {
